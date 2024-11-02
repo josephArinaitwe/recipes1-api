d19eccfcad7b75d44ee553c177d1f6327f533592
Kiira Joseph <jarinaitwe@kiiramotors.com>
2024-11-02 22:05:22 +0300

    list recipes endpoint

--- main.go
+++ main.go
@@ -1,28 +1,35 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"time"
-	"github.com/rs/xid"
+
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
 var recipes []Recipe
-func init(){
-	recipes = make([]Recipe,0)
-}
-type Recipe struct{
-	Name string `json: "name"`
-	Tags []string `json:"tags"`
-	ID string `json:"ID"`
-	Ingredients []string `json:"ingredients"`
-	Instructions []string `json:"instructions"`
-	PublishedAt time.Time `json:"publishedAt"`
+
+func init() {
+	recipes = make([]Recipe, 0)
+	file, _ := ioutil.ReadFile("recipes.json")
+	_= json.Unmarshal([]byte(file), &recipes)
 }
 
-func NewRecipeHandler(c *gin.Context){
+type Recipe struct {
+	Name         string    `json: "name"`
+	Tags         []string  `json:"tags"`
+	ID           string    `json:"ID"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
+
+func NewRecipeHandler(c *gin.Context) {
 	var recipe Recipe
-	if err := c.ShouldBindJSON(&recipe); err != nil{
+	if err := c.ShouldBindJSON(&recipe); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error()})
 		return
@@ -33,9 +40,12 @@ func NewRecipeHandler(c *gin.Context){
 	c.JSON(http.StatusOK, recipe)
 }
 
+func ListRecipesHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, recipes)
+}
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
-	router.Run()
+	router.GET("/recipes", ListRecipesHandler)
+	router.Run(":8081")
 }
-
